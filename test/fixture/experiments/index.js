a7cf14334e6fa53eb416eaec7b5ce624886ed818
lengjing <>
2020-09-23 15:22:20 +0800

    feat: support for several experiments

--- test/fixture/experiments/index.js
+++ test/fixture/experiments/index.js
@@ -1,9 +1,11 @@
-export default [{
-  name: 'test1',
-  experimentID: 'id1',
-  variants: [
-    { weight: 100 },
-    { weight: 0 }
-  ],
-  maxAge: 120
-}]
+export default [
+  {
+    name: 'test1',
+    experimentID: 'id1',
+    variants: [
+      { weight: 100 },
+      { weight: 0 }
+    ],
+    maxAge: 120
+  }
+]
